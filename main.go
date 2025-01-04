53f09dd2915043cc2428725e2d8d0f8b72b781bb
akira <asa.shakira340@gmail.com>
2025-01-04 16:09:29 +0900

    chore: get player rating for test purpose

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello")
+	m := New()
+	segaID := os.Getenv("SEGAID")
+	password := os.Getenv("PASSWORD")
+	err := m.Login(segaID, password)
+	if err != nil {
+		log.Println("maimai Login error: ", err)
+		return
+	}
+
+	res, err := m.HttpClient.Get(maimaiUrl + "/playerData")
+	if err != nil {
+		return
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return
+	}
+	rating, _ := strconv.Atoi(doc.Find(".rating_block").Text())
+	fmt.Println(rating)
 }
